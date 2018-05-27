bcf47f738a5947d2df63e294ae2fb9b9208f276b
Gabriela Mendes <contato@gabrielamendesp.com.br>
2018-05-27 13:35:29 -0300

    Exemplos de inscrição e remoçao de event listeners

--- src/app/browser-event-experiments/browser-event-experiments.component.ts
+++ src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrowserEventExperimentsComponent implements OnInit {
 
+  public hoverEvent: HTMLElement;
+
   constructor() { }
 
   ngOnInit() {
+    this.hoverEvent = document.getElementById('hover');
+    this.hoverEvent.addEventListener('mousemove', this.mouseEvento);
   }
 
+  public mouseEvento(ev: MouseEvent) {
+    console.log(ev);
+  }
+
+  public unsubscribe() {
+    console.log('tirando inscrição do evento mouseMove');
+    this.hoverEvent.removeEventListener('mousemove', this.mouseEvento);
+  }
 }
